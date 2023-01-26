0e1a4507303144277b497d9cfb53ae56771b3863
Josh Cole <colemangrill@live.com>
2023-01-25 16:49:05 -0800

    Style changes

--- src/serio.rs
+++ src/serio.rs
@@ -1,24 +1,24 @@
 //! This module represents the serial communication protocol
 //! based on UART physical hardware.
-//! 
+//!
 //! Typical operations in the kernel do not require direct uart access.
 //! Instead, the `serio` interface has been devised which abstracts
 //! much of the nuance away.
-//! 
+//!
 //! On the Teensy4.0, Uart6 is what most would think of as the "Primary" uart.
 //! It is located on pins 0 and 1.
-//! 
-//! It is worth noting that the debug module of this kernel leverages 
+//!
+//! It is worth noting that the debug module of this kernel leverages
 //! SerioDevice::Uart4 to output any debug data.
-//! 
+//!
 //! Simple usage
-//! 
+//!
 //! ```no_run
 //! use teensycore::serio::*;
-//! 
+//!
 //! serial_init(SerioDevice::Uart6);
 //! serial_write(SerioDevice::Uart6, b"Hello, world!\r\n");
-//! 
+//!
 //! while serial_available(SerioDevice::Uart6) > 0 {
 //!     let sb = serial_read(SerioDevice::Uart6);
 //!     // Do something with the Str
@@ -28,13 +28,13 @@
 #![allow(unused)]
 
 use crate::debug::*;
-use crate::phys::uart::*;
+use crate::phys::addrs;
 use crate::phys::irq::*;
 use crate::phys::pins::*;
-use crate::phys::addrs;
+use crate::phys::uart::*;
 use crate::system::buffer::*;
-use crate::system::vector::*;
 use crate::system::str::*;
+use crate::system::vector::*;
 
 struct HardwareConfig {
     device: Device,
@@ -46,7 +46,7 @@ struct HardwareConfig {
 }
 
 /// Enable this to mirror all bytes received
-/// to the Debug UART peripherla.
+/// to the Debug UART peripheral.
 const DEBUG_SPY: bool = false;
 static mut TEMP_BUF: [u8; 128] = [0; 128];
 const UART_WATERMARK_SIZE: u32 = 0x2;
@@ -58,66 +58,66 @@ static mut UART1: Uart = Uart::new(HardwareConfig {
     irq: Irq::Uart1,
     sel_inp_reg: None,
     sel_inp_val: None,
- });
- 
- static mut UART2: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart2, 
-    tx_pin: 14, 
-    rx_pin: 15, 
+});
+
+static mut UART2: Uart = Uart::new(HardwareConfig {
+    device: Device::Uart2,
+    tx_pin: 14,
+    rx_pin: 15,
     irq: Irq::Uart2,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART2_RX_SELECT_INPUT),
     sel_inp_val: Some(0x1),
- });
+});
 
 static mut UART3: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart3, 
-    tx_pin: 17, 
-    rx_pin: 16, 
+    device: Device::Uart3,
+    tx_pin: 17,
+    rx_pin: 16,
     irq: Irq::Uart3,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART3_RX_SELECT_INPUT),
     sel_inp_val: Some(0x0),
 });
 
 static mut UART4: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart4, 
-    tx_pin: 8, 
-    rx_pin: 7, 
+    device: Device::Uart4,
+    tx_pin: 8,
+    rx_pin: 7,
     irq: Irq::Uart4,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART4_RX_SELECT_INPUT),
     sel_inp_val: Some(0x2),
 });
 
 static mut UART5: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart5, 
-    tx_pin: 1, 
-    rx_pin: 0, 
+    device: Device::Uart5,
+    tx_pin: 1,
+    rx_pin: 0,
     irq: Irq::Uart5,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART5_RX_SELECT_INPUT),
     sel_inp_val: Some(0x0),
 }); // NOTE: THIS DEVICE DOESN'T HAVE VALID PINS
 
 static mut UART6: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart6, 
-    tx_pin: 1, 
-    rx_pin: 0, 
+    device: Device::Uart6,
+    tx_pin: 1,
+    rx_pin: 0,
     irq: Irq::Uart6,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART6_RX_SELECT_INPUT),
     sel_inp_val: Some(0x1),
 });
 
 static mut UART7: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart7, 
-    tx_pin: 29, 
-    rx_pin: 28, 
+    device: Device::Uart7,
+    tx_pin: 29,
+    rx_pin: 28,
     irq: Irq::Uart7,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART7_RX_SELECT_INPUT),
     sel_inp_val: Some(0x1),
 });
 
 static mut UART8: Uart = Uart::new(HardwareConfig {
-    device: Device::Uart8, 
-    tx_pin: 20, 
-    rx_pin: 21, 
+    device: Device::Uart8,
+    tx_pin: 20,
+    rx_pin: 21,
     irq: Irq::Uart8,
     sel_inp_reg: Some(addrs::IOMUXC_LPUART8_RX_SELECT_INPUT),
     sel_inp_val: Some(0x0),
@@ -137,7 +137,7 @@ pub enum SerioDevice {
     Debug = 0x9,
 }
 
-/** 
+/**
     This encapsulates an entire Uart device
     being instantiated, including all necessary memory
     and mappings.
@@ -148,7 +148,7 @@ struct Uart {
     rx_pin: usize,
     initialized: bool,
     irq: Irq,
-    tx_buffer: Buffer::<UART_BUFFER_DEPTH, u8>,
+    tx_buffer: Buffer<UART_BUFFER_DEPTH, u8>,
     rx_buffer: Str,
     sel_inp_reg: Option<u32>,
     sel_inp_val: Option<u32>,
@@ -161,7 +161,10 @@ impl Uart {
     pub const fn new(config: HardwareConfig) -> Uart {
         return Uart {
             device: config.device,
-            tx_buffer: Buffer { data: [0; UART_BUFFER_DEPTH], tail: 0 },
+            tx_buffer: Buffer {
+                data: [0; UART_BUFFER_DEPTH],
+                tail: 0,
+            },
             rx_buffer: Str::new(),
             buffer_head: 0,
             initialized: false,
@@ -172,80 +175,91 @@ impl Uart {
             irq: config.irq,
             tx_count: 0,
             paused: false,
-        }
+        };
     }
 
     fn initialize(&mut self) {
         if self.initialized {
             return;
         }
-        
+
         // Initialize the pins
         pin_mux_config(self.tx_pin, Alt::Alt2);
-        pin_pad_config(self.tx_pin, PadConfig {
-            hysterisis: true,
-            resistance: PullUpDown::PullDown100k,
-            pull_keep: PullKeep::Keeper,
-            pull_keep_en: false,
-            open_drain: false,
-            speed: PinSpeed::Max200MHz,
-            drive_strength: DriveStrength::MaxDiv3,
-            fast_slew_rate: true,
-        });
+        pin_pad_config(
+            self.tx_pin,
+            PadConfig {
+                hysterisis: true,
+                resistance: PullUpDown::PullDown100k,
+                pull_keep: PullKeep::Keeper,
+                pull_keep_en: false,
+                open_drain: false,
+                speed: PinSpeed::Low50MHz,
+                drive_strength: DriveStrength::MaxDiv3,
+                fast_slew_rate: true,
+            },
+        );
 
         pin_mux_config(self.rx_pin, Alt::Alt2);
-        pin_pad_config(self.rx_pin, PadConfig {
-            hysterisis: true,
-            resistance: PullUpDown::PullUp22k,
-            pull_keep: PullKeep::Pull,
-            pull_keep_en: true,
-            open_drain: false,
-            speed: PinSpeed::Low50MHz,
-            drive_strength: DriveStrength::Max,
-            fast_slew_rate: false,
-        });
+        pin_pad_config(
+            self.rx_pin,
+            PadConfig {
+                hysterisis: true,
+                resistance: PullUpDown::PullUp22k,
+                pull_keep: PullKeep::Pull,
+                pull_keep_en: true,
+                open_drain: false,
+                speed: PinSpeed::Low50MHz,
+                drive_strength: DriveStrength::MaxDiv3,
+                fast_slew_rate: false,
+            },
+        );
 
         // Configure the base settings
         uart_disable(self.device);
         uart_sw_reset(self.device, true);
         uart_sw_reset(self.device, false);
-        uart_configure(self.device, UartConfig {
-            r9t8: false,
-            invert_transmission_polarity: false,
-            overrun_irq_en: true,
-            noise_error_irq_en: false,
-            framing_error_irq_en: false,
-            parity_error_irq_en: false,
-            tx_irq_en: false, // This gets set later
-            rx_irq_en: true,
-            tx_complete_irq_en: true,
-            idle_line_irq_en: true,
-            tx_en: false,
-            rx_en: false,
-            match1_irq_en: false,
-            match2_irq_en: false,
-            idle_config: IdleConfiguration::Idle64Char,
-            doze_en: false,
-            bit_mode: BitMode::EightBits,
-            parity_en: false,
-            parity_type: ParityType::Even,
-        });
-    
-        uart_configure_fifo(self.device, FifoConfig {
-            tx_fifo_underflow_flag: false,
-            rx_fifo_underflow_flag: false,
-            tx_flush: false,
-            rx_flush: false,
-            tx_fifo_overflow_irq_en: false,
-            rx_fifo_underflow_irq_en: true,
-            tx_fifo_en: true,
-            rx_fifo_en: true,
-        });
+        uart_configure(
+            self.device,
+            UartConfig {
+                r9t8: false,
+                invert_transmission_polarity: false,
+                overrun_irq_en: true,
+                noise_error_irq_en: false,
+                framing_error_irq_en: false,
+                parity_error_irq_en: false,
+                tx_irq_en: false, // This gets set later
+                rx_irq_en: true,
+                tx_complete_irq_en: true,
+                idle_line_irq_en: true,
+                tx_en: false,
+                rx_en: false,
+                match1_irq_en: false,
+                match2_irq_en: false,
+                idle_config: IdleConfiguration::Idle64Char,
+                doze_en: false,
+                bit_mode: BitMode::EightBits,
+                parity_en: false,
+                parity_type: ParityType::Even,
+            },
+        );
 
+        uart_configure_fifo(
+            self.device,
+            FifoConfig {
+                tx_fifo_underflow_flag: false,
+                rx_fifo_underflow_flag: false,
+                tx_flush: false,
+                rx_flush: false,
+                tx_fifo_overflow_irq_en: false,
+                rx_fifo_underflow_irq_en: true,
+                tx_fifo_en: true,
+                rx_fifo_en: true,
+            },
+        );
 
         uart_set_pin_config(self.device, InputTrigger::Disabled);
         uart_disable_fifo(self.device);
-        
+
         uart_watermark(self.device, UART_WATERMARK_SIZE);
         uart_enable(self.device);
 
@@ -258,14 +272,13 @@ impl Uart {
         }
 
         pin_out(self.tx_pin, Power::Low);
-        
+
         irq_attach(self.irq, serio_handle_irq);
         irq_enable(self.irq);
         irq_priority(self.irq, 128);
         uart_baud_rate(self.device, 115200);
 
-
-        self.initialized = true;        
+        self.initialized = true;
     }
 
     pub fn available(&self) -> usize {
@@ -281,10 +294,10 @@ impl Uart {
     }
 
     pub fn write(&mut self, bytes: &[u8]) {
-        for byte_idx in 0 .. bytes.len() {
+        for byte_idx in 0..bytes.len() {
             self.tx_buffer.enqueue(bytes[byte_idx]);
         }
-        
+
         uart_set_reg(self.device, &CTRL_TCIE);
         pin_out(self.tx_pin, Power::High);
     }
@@ -304,7 +317,7 @@ impl Uart {
 
     fn handle_receive_irq(&mut self) {
         let irq_statuses = uart_get_irq_statuses(self.device);
-        
+
         // TODO: Implement some logic for these edge cases
         // but it's really not needed for just simply
         // receiving messages.
@@ -312,7 +325,7 @@ impl Uart {
         // let rx_active = irq_statuses & (0x1 << 24) > 0;
         // let rx_buffer_full = irq_statuses & (0x1 << 21) > 0;
         // let rx_idle = irq_statuses & (0x1 << 20) > 0;
-        
+
         // Read until it is empty
         let mut count = 0;
         while uart_has_data(self.device) {
@@ -323,7 +336,7 @@ impl Uart {
         }
 
         if DEBUG_SPY {
-            for idx in 0 .. count {
+            for idx in 0..count {
                 serial_write(SerioDevice::Debug, &[unsafe { TEMP_BUF[idx] }]);
             }
         }
@@ -332,11 +345,10 @@ impl Uart {
             crate::debug::blink_accumulate();
         }
     }
-    
+
     fn transmit(&mut self) {
         match self.tx_buffer.dequeue() {
-            None => {
-            },
+            None => {}
             Some(byte) => {
                 // Get the next byte to write and beam it
                 uart_write_fifo(self.device, byte);
@@ -353,8 +365,7 @@ impl Uart {
         // Check if there is space in the buffer
         if pending_data && tx_complete {
             self.transmit();
-        }
-         else if !pending_data {
+        } else if !pending_data {
             uart_clear_reg(self.device, &CTRL_TCIE);
         }
     }
@@ -374,7 +385,7 @@ impl Uart {
     }
 }
 
-fn get_uart_interface (device: SerioDevice) -> &'static mut Uart {
+fn get_uart_interface(device: SerioDevice) -> &'static mut Uart {
     unsafe {
         return match device {
             SerioDevice::Uart1 => &mut UART1,
@@ -406,7 +417,7 @@ pub fn serial_init(device: SerioDevice) {
 
 /// Retuns the current buffer of data the serial interface
 /// has accumulated.
-/// 
+///
 /// You can interact with this buffer, modify it, etc. It is
 /// recommended to call `.clear()` on the buffer as soon as you are
 /// done with the data, otherwise the buffer will eventually
@@ -423,7 +434,7 @@ pub fn serial_available(device: SerioDevice) -> usize {
 }
 
 /// Enqueue data to be written over serial.
-/// 
+///
 /// This data will be written at the next available interrupt
 /// cycle.
 pub fn serial_write(device: SerioDevice, bytes: &[u8]) {
@@ -440,7 +451,7 @@ pub fn serial_write_str(device: SerioDevice, bytes: &mut Str) {
     // Fixes memory leak. When calling this function you'll
     // usually be operating with an intermediary string and
     // won't be able to drop() it yourself.
-    bytes.drop();    
+    bytes.drop();
 }
 
 pub fn serial_baud(device: SerioDevice, rate: u32) {
