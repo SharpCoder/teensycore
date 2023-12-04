8bd5535aacf34af972a07a849f02c8160741a96f
SharpCoder <colemangrill@live.com>
2023-12-03 19:32:07 -0800

    Update the build template so it passes in a new flag that allows builds to work on more recent nightly

--- build-template.sh
+++ build-template.sh
@@ -10,7 +10,7 @@ then
 fi
 
 # Build with cargo
-RUSTFLAGS="-C panic=abort -C opt-level=2 -C no-redzone" cargo build --target thumbv7em-none-eabihf
+RUSTFLAGS="-C panic=abort -C opt-level=2 -C no-redzone -Zcross-crate-inline-threshold=0" cargo build --target thumbv7em-none-eabihf
 
 # Extract all projects in the workspace
 # and then build them into individual hex files
